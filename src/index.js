0a638cfcefbb5b0b944afca1b774834be7c972b5
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-30 12:35:44 -0300

    chore: Error handling http500 added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+require('express-async-errors')
 
 const routes = require('./routes')
 
@@ -6,5 +7,10 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
-
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log('###### ERROR HANDLER #####')
+  console.log(error)
+  response.sendStatus(500)
+})
 app.listen(3000, () => console.log('🔥 - Server starded at http://localhost:3000'))
